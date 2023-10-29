d8174ef0b1185c68f4a9d49c500b9f5bd3bf5aed
Jedo de la Pena II <jtdelapenaii@gmail.com>
2023-10-29 15:08:10 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+
+  let path = "./";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+
+    case "/contact":
+      path += "contact.html";
+      res.statusCode = 200;
+      break;
+
+    case "/about":
+      path += "about-me.html";
+      res.statusCode = 200;
+      break;
+
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end();
+      break;
+
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log("ERROR: ", err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+});
+
+server.listen(8080, "localhost", () => {
+  console.log("listening for request on port 8080");
+});
